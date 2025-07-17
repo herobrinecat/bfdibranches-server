36c4832b8703252f3012f6b49a0387960d48cd91
herobrinecat <alexfullflight@gmail.com>
2025-07-17 14:17:26 -0500

    seperated secret from code and added as a settings

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var verbose = false //Logs more info
 var trolladminurl = true //Trolls people by rickrolling when someone tries to go to /admin
 var blockOtherUserAgent = true //Block other user agents except Godot (make it more accurate to the server)
 var disableInventoryCheck = false //Disable the inventory check when purchasing PFPs
-
+var secret = "bfdibranchessecrettestthatis256b" //A secret when signing/checking the credentials on signup/login
 //variables
 const app = express()
 var shopitems = '{ "fg": { "69": 1000, "73": 500, "24": 500, "25": 500, "34": 500, "30": 500, "43": 500, "31": 500, "41": 500, "57": 500, "32": 500, "42": 500, "33": 500, "36": 500, "72": 500, "78": 500, "84": 500, "7": 1000, "18": 1000, "8": 1000, "16": 1000, "5": 1000, "17": 1000, "15": 1000, "11": 1000, "9": 1000, "14": 1000, "19": 1000, "12": 1000, "20": 1000, "6": 1000, "13": 1000, "10": 1000, "21": 1000, "62": 1000, "65": 1000, "66": 1000, "68": 1000, "67": 1000, "4": 4000, "40": 4000, "22": 4000, "63": 4000, "37": 7000, "35": 7000, "1010": 7000, "44": 15000, "59": 7000, "61": 7000, "64": 7000, "83": 7000 }, "bg": { "8": 500, "9": 500, "10": 500, "11": 500, "12": 500, "33": 500, "34": 500, "35": 500, "37": 500, "36": 500, "38": 500, "39": 500, "13": 1000, "14": 1000, "20": 1000, "24": 1000, "21": 1000, "22": 1000, "23": 1000, "42": 1000, "43": 1000, "48": 1000, "49": 1000, "66": 1000, "67": 1000, "68": 1000, "69": 1000, "32": 1500, "15": 1500, "16": 1500, "17": 1500, "64": 1500, "18": 1500, "19": 1500, "25": 1500, "26": 1500, "27": 1500, "28": 1500, "29": 1500, "30": 1500, "31": 1500, "44": 1500, "45": 1500, "46": 1500, "70": 1500, "71": 1500, "65": 1500 } }'
@@ -1664,7 +1664,7 @@ if (blockOtherUserAgent == false || req.headers["user-agent"] != undefined && re
         const [results1, field1] = await connection.query("INSERT INTO bfdibranchesaccount (username, password, date, lastonline) VALUES (?, ?, ?, ?)",[req.body["username"],req.body["password"],"Account Created: " + datet.getFullYear() + "-" + ("0" + (parseInt(datet.getMonth()) + 1).toString()).slice(-2) + "-" + datet.getDate() + " " + datet.getHours() + ":" + datet.getMinutes(), datet.getFullYear() + "-" + ("0" + (parseInt(datet.getMonth()) + 1).toString()).slice(-2) + "-" + datet.getDate() + " " + datet.getHours() + ":" + datet.getMinutes()])
 
         if (results1["affectedRows"] > 0) {
-              var token = jwt.sign({"password":req.body["password"]}, 'bfdibranchessecrettestthatis256b',{algorithm: 'HS256'})
+              var token = jwt.sign({"password":req.body["password"]}, secret,{algorithm: 'HS256'})
         res.set({
             'X-Powered-By': 'Express',
             'Content-Type': 'text/html; charset=utf-8',
@@ -1705,7 +1705,7 @@ app.post("/login.php", async (req, res) => {
         'SELECT id, username, password FROM bfdibranchesaccount WHERE username = ? AND password = ?',[req.body["username"], req.body["password"]]
     );
     if (JSON.stringify(results) != "[]") {
-        var token = jwt.sign({"password":req.body["password"]}, 'bfdibranchessecrettestthatis256b',{algorithm: 'HS256'})
+        var token = jwt.sign({"password":req.body["password"]}, secret,{algorithm: 'HS256'})
         res.set({
             'X-Powered-By': 'Express',
             'Content-Type': 'text/html; charset=utf-8',
@@ -1750,7 +1750,7 @@ function parseJwt (token) {
         var result = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
 
         if (disableSignatureCheck == false) {
-        var checktoken = jwt.sign(result, "bfdibranchessecrettestthatis256b", { algorithm: "HS256" })
+        var checktoken = jwt.sign(result, secret, { algorithm: "HS256" })
 
         if (checktoken == token) {
             return result
@@ -2292,7 +2292,7 @@ app.post("/changeaccountinfo.php", async (req, res) => {
         if (results1["affectedRows"] > 0) {
             const [results2, fields2] = await connection.query("SELECT id FROM bfdibranchesaccount WHERE username = ?", [req.body["newusername"]])
             if (results2.length > 0) {
-                      var token = jwt.sign({"password":req.body["password"]}, 'bfdibranchessecrettestthatis256b',{algorithm: 'HS256'})
+                      var token = jwt.sign({"password":req.body["password"]}, secret,{algorithm: 'HS256'})
         res.set({
             'X-Powered-By': 'Express',
             'Content-Type': 'text/html; charset=utf-8',
